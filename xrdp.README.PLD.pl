86cb57905cff0de1c8fd8199730735689feda33a
pawelz <pawelz@pld-linux.org>
2008-07-02 17:07:42 +0000

    - PLD notes
    Changed files:
        xrdp.README.PLD -> 1.1
        xrdp.README.PLD.pl -> 1.1

---
+++ xrdp.README.PLD.pl
@@ -0,0 +1,17 @@
+=== Sesja: ===
+
+Sesman, uruchamiając sesję użytkownika, korzysta ze skryptu
+/etc/X11/xinit/Xclients. Dlatego, aby zmienić manager okien, najprościej
+wpisać odpowiedni program w pliku ~/.desktop
+
+Mozna również wskazać inny skrypt uruchamiający sesję Xów w konfiguracji
+sesmana: /etc/xrdp/sesman.ini.
+
+=== Uprawnieni użytkownicy: ===
+
+Aby ograniczyć prawo do korzystania z xrdp do określonej grupy uzytkowników,
+można wpisać tą grupę w pliku /etc/xrdp/sesman.ini w polu TerminalServerUsers.
+
+=== Znane błędy: ===
+
+Sesman nie potrafi uruchomić Xvnc, jeżeli pam korzysta z LDAPa.
